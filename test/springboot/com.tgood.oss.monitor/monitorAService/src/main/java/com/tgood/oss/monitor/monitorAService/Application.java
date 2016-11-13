23d4c2efa38e32d5b922c9400a1209f660247c80
tongyijie <tongyijie@163.com>
2016-11-13 23:12:23 +0800

    基于tomcat部署springboot

---
+++ test/springboot/com.tgood.oss.monitor/monitorAService/src/main/java/com/tgood/oss/monitor/monitorAService/Application.java
@@ -0,0 +1,21 @@
+package com.tgood.oss.monitor.monitorAService;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+}
---
+++ test/springboot/com.tgood.oss.platform/registerservice/src/main/java/com/tgood/oss/platform/registerservice/Application.java
@@ -0,0 +1,21 @@
+package com.tgood.oss.platform.registerservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+}
